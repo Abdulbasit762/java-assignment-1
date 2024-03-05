997ecbe15009ccfc822eff062248199889f5c516
Abdulbasit762 <154410960+Abdulbasit762@users.noreply.github.com>
2024-03-06 03:42:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// console.log(prompt(""))_
+
+
+// var num1 = 4
+// var num2 = "4"
+
+// if(4==="4"){
+//     console.log("true")
+// }else{
+//     console.log("false")
+// }
+
+///////         Assignment no 1
+
+alert("Error! Please enter a valid password")
+alert("Welcome to JS Land...\nHappy Coding")
+alert("Welcome to JS Land...")
+alert("Happy Coding! \n Prevent this page from creating additional dialogs.")
+alert("Hello...I can run JS through my web browser's console")
